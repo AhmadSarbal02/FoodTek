fee35a405e87831d1854cdf47393c1ba64b6571e
AntonAnwar <antonanwar22@gmail.com>
2025-03-24 10:31:06 +0300

    main pages

---
+++ lib/view/screen/main_screens/home/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:foodtek/view/screen/onBoarding_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => OnboardingScreen()),
+            );
+          },
+          child: Text("Go to the OnBoarding Screen"),
+        ),
+      ),
+    );
+  }
+}
